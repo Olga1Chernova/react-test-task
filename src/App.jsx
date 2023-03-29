391243390b3184d4a9ce10e3c4ac271aec5d4b53
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2023-03-29 16:39:44 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from 'redux/store';
-
-import UserRoutes from './UserRoutes';
 
 function App() {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter basename="/react-test-task">
-            <UserRoutes />
-          </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    <></>
   );
 }
 
