c0ac55d07d33af6a96ff372be19bd4b19bb7b9c0
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2023-04-03 03:46:33 +0300

    created AddProductPage

--- src/pages/AddProductPage/AddProductPage.jsx
+++ src/pages/AddProductPage/AddProductPage.jsx
@@ -1,8 +1,20 @@
+import { useDispatch } from "react-redux";
+
+import {fetchAddProduct} from 'redux/products/products-operations';
+
+
 import AddProductForm from "modules/AddProductForm/AddProductForm";
 
 const AddProductPage = () => {
+    const dispatch = useDispatch();
+
+  const onAddProduct = ({ name, author, year, category }) => {
+      const action = fetchAddProduct({ name, author, year, category });
+      dispatch(action);
+    };
+
     return (
-        <AddProductForm />
+        <AddProductForm onSubmit={onAddProduct}/>
     )
 }
 export default AddProductPage;
