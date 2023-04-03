3c4883c667339ccbd3492445ac81e5d0610d876a
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2023-04-03 03:44:56 +0300

    created form with formik and yup

--- src/modules/AddProductForm/AddProductForm.jsx
+++ src/modules/AddProductForm/AddProductForm.jsx
@@ -4,47 +4,121 @@ import * as Yup from 'yup';
 import css from './add-product-form.module.scss'
 
 const initialValues = {
-  name: '',
-  brand: '',
+  title: '',
+  author: '',
+  year: '',
   rating: '',
+  category: '',
 };
 
 const validationSchema = Yup.object().shape({
-  name: Yup.string().required('Required'),
-  brand: Yup.string().required('Required'),
+  title: Yup.string().required('Required'),
+  author: Yup.string().required('Required'),
+  category: Yup.string().required('Required'),
+  year: Yup.number()
+    .min(1900, 'Must be greater than 1900')
+    .max(
+      new Date().getFullYear(),
+      `Must be less than or equal to ${new Date().getFullYear()}`
+    )
+    .required('Required'),
   rating: Yup.number()
     .min(0, 'Must be greater than or equal to 0')
     .max(5, 'Must be less than or equal to 5')
     .required('Required'),
 });
 
-function AddProductForm({ handleSubmit }) {
+function AddProductForm({ onSubmit }) {
+  
+  const handleSubmit = (event, formik) => {
+    event.preventDefault();
+    onSubmit(formik.values);
+    formik.resetForm();
+  };
+
   return (
     <div>
-      <h2>Add a Product</h2>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={handleSubmit}
-      >
+      <h2 className={css.title}>Fill in a form to add a product</h2>
+      <Formik initialValues={initialValues} validationSchema={validationSchema}>
         {formik => (
-          <Form>
+          <Form className={css.form} onSubmit={e => handleSubmit(e, formik)}>
             <div>
-              <label htmlFor="name">Name</label>
-              <Field type="text" name="name" id="name" />
-              <ErrorMessage name="name" component="div" className="error" />
+              <label htmlFor="title" className={css.label}>
+                Title
+              </label>
+              <Field type="text" name="title" id="name" className={css.field} />
+              <ErrorMessage
+                name="title"
+                component="div"
+                className={css.error}
+              />
             </div>
             <div>
-              <label htmlFor="brand">Brand</label>
-              <Field type="text" name="brand" id="brand" />
-              <ErrorMessage name="brand" component="div" className="error" />
+              <label htmlFor="author" className={css.label}>
+                Author
+              </label>
+              <Field
+                type="text"
+                name="author"
+                id="author"
+                className={css.field}
+              />
+              <ErrorMessage
+                name="author"
+                component="div"
+                className={css.error}
+              />
             </div>
             <div>
-              <label htmlFor="rating">Rating</label>
-                <Field type="number" name="rating" id="rating" step={0.1} max={5} min={0} />
-              <ErrorMessage name="rating" component="div" className="error" />
+              <label htmlFor="category" className={css.label}>
+                Category
+              </label>
+              <Field
+                type="text"
+                name="category"
+                id="category"
+                className={css.field}
+              />
+              <ErrorMessage
+                name="category"
+                component="div"
+                className={css.error}
+              />
             </div>
-            <button type="submit">Add Product</button>
+            <div>
+              <label htmlFor="year" className={css.label}>
+                Year
+              </label>
+              <Field
+                type="number"
+                name="year"
+                id="year"
+                className={css.field}
+              />
+              <ErrorMessage name="year" component="div" className={css.error} />
+            </div>
+            <div>
+              <label htmlFor="rating" className={css.label}>
+                Rating
+              </label>
+              <Field
+                type="number"
+                name="rating"
+                id="rating"
+                step={0.1}
+                max={5}
+                min={0}
+                className={css.field}
+              />
+              <ErrorMessage
+                name="rating"
+                component="div"
+                className={css.error}
+              />
+            </div>
+            <button type="submit" className={css.button}>
+              Add Product
+            </button>
           </Form>
         )}
       </Formik>
