57b7a83954e4b80234cc4df4b5df221a67fe838a
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2023-04-03 03:46:52 +0300

    created api

--- src/shared/services/products-api.js
+++ src/shared/services/products-api.js
@@ -4,12 +4,12 @@ const instance = axios.create({
     baseURL: 'https://dummyjson.com'
 });
 
-export const getAllProducts = async() => {
-    const { data } = await instance.get("/products");
-    return data;
-}
-
 export const addProduct = async (data) => {
     const { data: result } = await instance.post("/products/add", data);
     return result;
+}
+
+export const deleteProduct = async (id) => {
+    const { data } = await instance.get(`/products/${id}`);
+    return data;
 }
