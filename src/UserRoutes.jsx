7da0ba5200315373276fdc2e65d65302413cf5ff
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2023-04-03 08:23:40 +0300

    Update UserRoutes.jsx

--- src/UserRoutes.jsx
+++ src/UserRoutes.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 
 import Loader from "shared/Loader/Loader";
 
-const Navigation = lazy(() => import("modules/Navigation/Navigation"));
 const HomePage = lazy(() => import("pages/HomePage/HomePage"));
 const AddProductPage = lazy(() => import("pages/AddProductPage/AddProductPage"));
 
